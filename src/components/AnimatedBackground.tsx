7eeb77f9b54e78d2ad02b38f93da17d10ebae9b7
Keith Fung <keith.robert.fung@gmail.com>
2023-01-26 20:46:15 -0500

    🎨 Blob Rework

--- src/components/AnimatedBackground.tsx
+++ src/components/AnimatedBackground.tsx
@@ -1,102 +1,127 @@
 /** @jsx jsx */
 import { jsx } from "theme-ui";
 
-import { FC } from "react";
-import { motion } from "framer-motion";
-import { Blob1, Blob2, Blob3, Blob4 } from "../svgs";
+import { FC, useEffect, useLayoutEffect, useRef, useState } from "react";
+import { motion, Transition } from "framer-motion";
+import { blobs } from "../svgs";
 
-export const AnimatedBackground: FC = () => (
-  <div
-    sx={{
-      left: 0,
-      zIndex: 1,
-      position: "absolute",
-      height: "100vh",
-      width: "100vw",
-      overflow: "hidden",
-    }}
-  >
-    <motion.div
-      style={{
+const BLOB_RATIO = 0.25;
+const BLOB_TRANSITION: Transition = {
+  repeatType: "reverse",
+  repeat: Infinity,
+  duration: 30,
+  rotate: 20,
+  ease: "linear",
+};
+
+const blobVariations = [
+  {
+    color: "watermelon",
+    startX: "20vw",
+    startY: "0vh",
+    endX: "100vw",
+    endY: "100vh",
+  },
+  {
+    color: "spiroDiscoBall",
+    startX: "20vw",
+    startY: "100vh",
+    endX: "80vw",
+    endY: "0vh",
+    ease: "easeInOut",
+  },
+  {
+    color: "verdigris",
+    startX: "80vw",
+    startY: "60vh",
+    endX: "20vw",
+    endY: "0vh",
+  },
+  {
+    color: "darkTopaz",
+    startX: "0vw",
+    startY: "30vh",
+    endX: "95vw",
+    endY: "20vh",
+  },
+];
+
+export const AnimatedBackground: FC = () => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  const [width, setWidth] = useState(0);
+  const [height, setHeight] = useState(0);
+  const blobHeight = height * BLOB_RATIO;
+
+  useLayoutEffect(() => {
+    if (ref.current) {
+      setWidth(ref.current.clientWidth);
+      setHeight(ref.current.clientHeight);
+    }
+  }, []);
+
+  useEffect(() => {
+    function handleWindowResize() {
+      if (ref.current) {
+        setWidth(ref.current.clientWidth);
+        setHeight(ref.current.clientHeight);
+        console.log(height);
+        console.log(width);
+      }
+    }
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={ref}
+      sx={{
         position: "absolute",
-        x: 200,
-        y: 800,
-      }}
-      animate={{
-        x: -100,
-        y: -500,
-        transition: {
-          repeatType: "reverse",
-          repeat: Infinity,
-          duration: 30,
-          rotate: 20,
-          ease: "linear",
-        },
+        width: "100vw",
+        height: "100vh",
+        zIndex: 1,
+        left: 0,
+        top: 0,
+        overflow: "hidden",
       }}
     >
-      <Blob1 sx={{ height: 400, color: "watermelon" }} />
-    </motion.div>
-    <motion.div
-      style={{
-        position: "absolute",
-        x: -200,
-        y: 400,
-      }}
-      animate={{
-        x: 800,
-        y: 500,
-        transition: {
-          repeatType: "reverse",
-          repeat: Infinity,
-          duration: 30,
-          rotate: 20,
-          ease: "linear",
-        },
-      }}
-    >
-      <Blob2 sx={{ height: 300, color: "spiroDiscoBall" }} />
-    </motion.div>
-    <motion.div
-      style={{
-        position: "absolute",
-        x: 1000,
-        y: 1000,
-      }}
-      animate={{
-        x: 1000,
-        y: 0,
-        transition: {
-          repeatType: "reverse",
-          repeat: Infinity,
-          duration: 30,
-          rotate: 20,
-          ease: "linear",
-        },
-      }}
-    >
-      <Blob3 sx={{ height: 400, color: "verdigris" }} />
-    </motion.div>
-    <motion.div
-      style={{
-        position: "absolute",
-        x: 200,
-        y: -400,
-      }}
-      animate={{
-        x: 200,
-        y: 800,
-        transition: {
-          repeatType: "reverse",
-          repeat: Infinity,
-          duration: 30,
-          rotate: 20,
-          ease: "linear",
-        },
-      }}
-    >
-      <Blob4 sx={{ height: 450, color: "darkTopaz" }} />
-    </motion.div>
-  </div>
-);
+      {blobs.map((Blob, index) => {
+        const { color, startX, startY, endX, endY, ease } =
+          blobVariations[index];
+        return (
+          <motion.div
+            style={{
+              position: "absolute",
+              maxWidth: "fit-content",
+            }}
+            initial={{
+              x: startX || "0vw",
+              y: startY || "0vh",
+            }}
+            animate={{
+              x: endX || "100vw",
+              y: endY || "100vh",
+              transition: BLOB_TRANSITION,
+            }}
+            transition={{
+              repeatType: "reverse",
+              repeat: Infinity,
+              duration: 30,
+              rotate: 20,
+              ease: ease || "linear",
+            }}
+          >
+            <Blob sx={{ height: blobHeight, color }} />
+          </motion.div>
+        );
+      })}
+    </div>
+  );
+};
 
 export default AnimatedBackground;
--- src/svgs/blobs/index.ts
+++ src/svgs/blobs/index.ts
@@ -1,4 +1,6 @@
-export { default as Blob1 } from "./blob-1.svg";
-export { default as Blob2 } from "./blob-2.svg";
-export { default as Blob3 } from "./blob-3.svg";
-export { default as Blob4 } from "./blob-4.svg";
+import { default as Blob1 } from "./blob-1.svg";
+import { default as Blob2 } from "./blob-2.svg";
+import { default as Blob3 } from "./blob-3.svg";
+import { default as Blob4 } from "./blob-4.svg";
+
+export const blobs = [Blob1, Blob2, Blob3, Blob4];
