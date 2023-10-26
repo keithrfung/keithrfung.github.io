8d8cc29ccad09a79a0685c34006387e5f8de9af8
Keith Fung <keith.robert.fung@gmail.com>
2023-10-26 16:16:47 -0400

    ♿️ Improve accessibility

--- src/components/portrait.tsx
+++ src/components/portrait.tsx
@@ -16,6 +16,7 @@ export const Portrait: FC = () => (
       transition={{ duration: 0.5 }}
       sx={{ width: "40%", clipPath: "circle(50% at center)" }}
       src="https://1.gravatar.com/avatar/2ed0a4ec4005392d9eb57e61d9efbbb2?size=800"
+      alt="Artistic Portrait of Keith Fung"
     />
   </Box>
 );
